3460f922889b01e42af4f883c7b3494cce81ce28
Katie Schrattenholzer <katieschratt@iMac.local>
2022-01-12 12:03:11 -0800

    added useColorPicker custom hook

--- src/hooks/useColorPicker.js
+++ src/hooks/useColorPicker.js
@@ -1,3 +1,15 @@
-export default function useColorPicker() {
+import { useState } from "react";
+
+export default function useColorPicker(initialState) {
   // TODO: Implement me!
+  const [state, setState] = useState(initialState);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setColorState((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
+  return [formState, handleChange]
 }
